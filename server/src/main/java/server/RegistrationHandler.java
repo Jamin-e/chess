3ed85e6874ec562d7c872ca4b9d4e262dfd1aa9b
Jamin Easton <jamine@byu.edu>
2026-02-28 02:57:26 -0700

    RegistrationHandler

---
+++ server/src/main/java/server/RegistrationHandler.java
@@ -0,0 +1,44 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import service.RegisterRequest;
+import service.RegisterResult;
+import service.UserService;
+
+
+public class RegistrationHandler {
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public RegistrationHandler(DataAccess dataAccess){
+        this.userService = new UserService(dataAccess);
+    }
+
+    public Handler login = ctx -> handleRegistration(ctx);
+
+    private void handleRegistration(Context ctx){
+        try{
+            var registerRequest = gson.fromJson(ctx.body(),RegisterRequest.class);
+
+            RegisterResult result = userService.register(registerRequest);
+
+            ctx.status(200);
+            ctx.json(result);
+        }
+        catch(DataAccessException e){}
+            String message = ctx.result();
+        if ("Error: username already taken".equals(message)) {
+            ctx.status(403);
+        }
+
+        ctx.json(new ErrorResponse(message));
+
+    }
+
+    private record ErrorResponse(String message) {}
+
+}
