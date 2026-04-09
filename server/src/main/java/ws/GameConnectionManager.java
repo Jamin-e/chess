4b220ea6ce878508404a11cff265132aee5f2945
Jamin Easton <jamine@byu.edu>
2026-04-09 01:53:24 -0600

    final skeleton work

--- server/src/main/java/ws/GameConnectionManager.java
+++ server/src/main/java/ws/GameConnectionManager.java
@@ -1,4 +1,24 @@
 package ws;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameConnectionManager {
+    private final Map<Integer,Object> gameConnections = new HashMap<>();
+
+    public void addConnection(Integer gameID, Object session){
+        //tbi
+    }
+
+    public void removeConnection(Integer gameID, Object session){
+        //tbi
+    }
+
+    public void broadcastToGame(Integer session, Object message){
+        //tbi
+    }
+
+    public void broadcastToRoot(Object session, Object message){
+        //tbi
+    }
 }
--- server/src/main/java/ws/GameService.java
+++ server/src/main/java/ws/GameService.java
@@ -1,4 +1,21 @@
 package ws;
 
+import websocket.commands.UserGameCommand;
+
 public class GameService {
+    public void handleConnect(UserGameCommand command){
+        //tbi
+    }
+
+    public void handleMakeMove(UserGameCommand command){
+        //tbi
+    }
+
+    public void handleLeave(UserGameCommand command){
+        //tbi
+    }
+
+    public void handleResign(UserGameCommand command){
+        //tbi
+    }
 }
